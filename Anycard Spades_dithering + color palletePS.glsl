ed30c88395af41a5bfc570d1197537ea41e76a60
Ares Kittell <akittell2025@francisparker.org>
2024-06-24 20:26:07 -0700

    added in color pallete compatability. Also you have to do save as for anything to actually get put in this folder, pretty annoying but oh well

--- Anycard Spades_dithering + color palletePS.glsl
+++ Anycard Spades_dithering + color palletePS.glsl
@@ -2,11 +2,20 @@
 
 //uniform vec2 uResolution;
 uniform sampler2D tex;
+uniform sampler2D tex2;
 
 out vec4 outColor;
 
 float GetNoise(mat4 mat, int x, int y) {
 	return float(mat[x % 4][y % 4]) * (1.0 / 16.0) - 0.5;
+}
+
+float posterize(float c, float level){
+	return floor((level - 1.) * c + 0.5) / (level - 1.);
+}
+
+vec4 posterize(vec4 c, float level){
+	return floor((level - 1.) * c + 0.5) / (level - 1.);
 }
 
 void main() {
@@ -24,14 +33,17 @@ void main() {
    
    vec4 texColor = texture(tex, uv.xy);
    
-   //this color vector is the actual final result of dithering
-   vec4 col = texColor + 0.5 * GetNoise(bayer4, int(uv.x * resolution.x), int(uv.y * resolution.y));
+   float grayscale = dot(texColor.rgb, vec3(0.21, 0.72, 0.07));
+   vec4 grayscaleColor = vec4(grayscale, grayscale, grayscale, 1.);
    
-   int numberOfReds = 2;
-   int numberOfGreens = 2;
-   int numberOfBlues = 2;
+   //this color vector is the actual final result of dithering
+   float ditherSpread = 0.05;
+   vec4 col = grayscaleColor + ditherSpread * GetNoise(bayer4, int(uv.x * resolution.x), int(uv.y * resolution.y));
+   
+   //posterization of colors
+   col = posterize(col, 5);
+   if(col.x > 0.) col = texture(tex2, vec2(col.x, 0));
+   else col = texture(tex2, vec2(0.999, 0));
    
    outColor = col;
-   
-   //outColor = vec4(0., (cos(uv.x + uv.y) + 1.) * 0.5, 1., 1.);
 }
