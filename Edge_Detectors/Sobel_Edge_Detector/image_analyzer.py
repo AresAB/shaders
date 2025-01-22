f9169326d44b48f13f9255651a91110b5e87c93d
Ares Kittell <akittell2025@francisparker.org>
2025-01-21 16:20:07 -0800

    fixed the screenshots not being save with the .jpg extension attatched

--- Edge_Detectors/Sobel_Edge_Detector/image_analyzer.py
+++ Edge_Detectors/Sobel_Edge_Detector/image_analyzer.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 def main():
-    scr_size = [1920, 1080]
+    scr_size = [1920 - 2, 1080 - 10]
 
     image_info = " ".join(sys.argv).split(",")
 
@@ -19,6 +19,6 @@ def main():
     image_dir = "results/" + image_info[-1].split("/")[-1]
     counter = "_" + str(len(os.listdir("./results")))
             
-    print(image_dimensions[0], image_dimensions[1], image_color, image_dir + counter)
+    print(image_dimensions[0], image_dimensions[1], image_color, image_dir + counter + ".jpg")
 
 main()
