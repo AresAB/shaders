5c47aea12c584bfb301c909f3e459ebd09870f3e
Ares Kittell <akittell2025@francisparker.org>
2025-07-28 13:02:30 -0700

    added a template folder to help for making new projects

---
+++ templates/Main_T1/src/shader_frag.fs
@@ -0,0 +1,12 @@
+#version 330 core
+
+layout(location = 0) out vec3 FragColor;
+
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+
+void main()
+{
+    FragColor = texture(texture1, TexCoord).xyz;
+}
