d193f3689c90fae428e57719eeca06a431dd7171
Acidic <ares.kittell@gmail.com>
2026-02-09 08:50:11 -0500

    added scene with spotlights

---
+++ Primitive_Lights/src/main.cpp
@@ -0,0 +1,448 @@
+#include <glad/glad.h>
+#include <glad/glad.c>
+#include <GLFW/glfw3.h>
+
+#define IMGUI_DEFINE_MATH_OPERATORS
+#define IMGUI_IMPL_OPENGL_LOADER_CUSTOM
+#include <imgui/imgui.h>
+#include <imgui/imgui_impl_glfw.h>
+#include <imgui/imgui_impl_opengl3.h>
+#include <imgui/imconfig.h>
+#include <imgui/imgui_internal.h>
+#include <imgui/imgui.cpp>
+#include <imgui/imgui_impl_glfw.cpp>
+#include <imgui/imgui_impl_opengl3.cpp>
+#include <imgui/imgui_draw.cpp>
+#include <imgui/imgui_tables.cpp>
+#include <imgui/imgui_widgets.cpp>
+#include <imgui/imgui_demo.cpp>
+
+#include <stb_image.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+#include <stb_image.cpp>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <shader_s.h>
+#include <quad_s.h>
+#include <cube_s.h>
+#include <table_s.h>
+
+#include <memory>
+#include <iostream>
+#include <cmath>
+
+unsigned int loadTexture(std::string filename, GLenum image_type);
+void processInput(GLFWwindow *window, glm::mat4& view_dir, glm::mat4& view_loc, float& fov, float& near_z, float& far_z);
+glm::mat4 makePerspectiveMatrix(float fov, float aspect_ratio, float n, float f);
+void saveScreenshotToFile(std::string filename, GLFWwindow *window, int renderedTexture, int scr_width, int scr_height);
+
+int main(int argc, char *argv[])
+{
+    // input handling
+    // --------------------
+    const unsigned int SCR_WIDTH = atoi(argv[1]);
+    const unsigned int SCR_HEIGHT = atoi(argv[2]);
+    const unsigned int SCR_SHOT_WIDTH = atoi(argv[3]);
+    const unsigned int SCR_SHOT_HEIGHT = atoi(argv[4]);
+    const char *SCR_SHOT_PATH = argv[5];
+
+    // setup
+    // --------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glEnable(GL_DEPTH_TEST);
+
+    // texture rendering buffer setup
+    // --------------------
+    unsigned int frameBuffer = 0;
+    glGenFramebuffers(1, &frameBuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
+
+    unsigned int depthBuffer;
+    glGenRenderbuffers(1, &depthBuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, depthBuffer);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, SCR_SHOT_WIDTH, SCR_SHOT_HEIGHT);
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthBuffer);
+
+    unsigned int renderedTexture;
+    glGenTextures(1, &renderedTexture);
+
+    glBindTexture(GL_TEXTURE_2D, renderedTexture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_SHOT_WIDTH, SCR_SHOT_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+    glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, renderedTexture, 0);
+
+    GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
+    glDrawBuffers(1, DrawBuffers);
+
+    if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+    return false;
+
+    // post processing shader setup
+    // --------------------
+    glBindTexture(GL_TEXTURE_2D, renderedTexture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    Shader post_shader("src/post_vert.vs", "src/post_frag.fs");
+    post_shader.use();
+    post_shader.setInt("scr_tex", 0);
+
+    Quad screen_quad = Quad();
+
+    // scene setup (only section (aside from render loop) you should be touching)
+    // --------------------
+    // texture generation
+    unsigned int texture1 = loadTexture("textures/container2.png", GL_RGBA);
+    unsigned int texture2 = loadTexture("textures/container2_specular.png", GL_RGBA);
+
+    // shader generation
+    Shader lightShader("src/light_vert.vs", "src/light_frag.fs");
+
+    Shader ourShader("src/shader_vert.vs", "src/shader_frag.fs"); // you can name your shader files however you like
+    ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
+    ourShader.setInt("texture1", 0);
+
+    // scene object(s) initialization
+    Table table = Table();
+    Cube cube = Cube();
+
+    // uniforms
+    glm::vec3 light1_pos;
+    glm::vec3 light1_col;
+    glm::vec3 light2_pos;
+    glm::vec3 light2_col;
+
+    glm::vec3 light3_pos = glm::vec3(1.5f, 0.5f, 0.5f);
+    ourShader.setVec3("spotlights[0].pos", light3_pos);
+    glm::vec3 light3_col = glm::vec3(0.f, 1.0f, 0.f);
+    ourShader.setFloat("spotlights[0].umbra", cos(50.f * 3.14f / 180.f));
+    ourShader.setFloat("spotlights[0].penumbra", cos(40.f * 3.14f / 180.f));
+    
+    ourShader.setInt("material.diffuse", 0);
+    ourShader.setInt("material.specular", 1);
+    ourShader.setVec3("pointlights[0].specular", glm::vec3(1.0f));
+    // for range : decay values ratio, this is a decent site
+    // https://wiki.ogre3d.org/tiki-index.php?page=-Point+pointlights[0]+Attenuation
+    ourShader.setFloat("pointlights[0].linear_decay", 0.35f);
+    ourShader.setFloat("pointlights[0].exp_decay", 0.44f);
+    ourShader.setVec3("pointlights[1].specular", glm::vec3(1.0f));
+    ourShader.setFloat("pointlights[1].linear_decay", 0.35f);
+    ourShader.setFloat("pointlights[1].exp_decay", 0.44f);
+
+    ourShader.setVec3("spotlights[0].ambient", glm::vec3(0.f));
+    ourShader.setVec3("spotlights[0].diffuse", light3_col * 0.9f);
+    ourShader.setVec3("spotlights[0].specular", glm::vec3(1.0f));
+    ourShader.setFloat("spotlights[0].linear_decay", 0.35f);
+    ourShader.setFloat("spotlights[0].exp_decay", 0.44f);
+
+    // rendering matricies setup
+    // --------------------
+    ourShader.use();
+    // create fov for perspective matrix calculation
+    float perspective_fov = 0.785f; // modify for different zoom levels
+    float near_z = 0.1f; // near plane z
+    float far_z = 100.f; // far plane z
+    glm::mat4 perspective = makePerspectiveMatrix(perspective_fov, (float)SCR_WIDTH / (float)SCR_HEIGHT, near_z, far_z);
+
+    // seperate view matrix components to allow for camera movement and rotation
+    glm::mat4 view_dir = glm::mat4(1.0f);
+    glm::mat4 view_loc = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -0.2f, -3.0f));
+    glm::mat4 view = view_dir * view_loc;
+
+    glm::mat4 model = glm::mat4(1.0f);
+    glm::mat4 lamp1_model;
+    glm::mat4 lamp2_model;
+
+    glm::mat4 table1_model = glm::translate(model, glm::vec3(0,-1.25,0));
+    glm::mat4 table1_normal = glm::transpose(glm::inverse(glm::mat3(table1_model)));
+
+    glm::mat4 cube1_model = glm::translate(model, glm::vec3(-0.5, 0, 0));
+    cube1_model = glm::scale(cube1_model, glm::vec3(0.25));
+    glm::mat4 cube1_normal = glm::transpose(glm::inverse(glm::mat3(cube1_model)));
+
+    glm::mat4 cube2_model = glm::translate(model, glm::vec3(0.5,0.25,-0.25));
+    cube2_model = glm::rotate(cube2_model, -0.4f, glm::vec3(0,1,0));
+    cube2_model = glm::scale(cube2_model, glm::vec3(0.4, 0.5, 0.1));
+    glm::mat4 cube2_normal = glm::transpose(glm::inverse(glm::mat3(cube2_model)));
+
+    glm::mat4 cube3_model = glm::translate(model, glm::vec3(0.55,-0.15,0.75));
+    cube3_model = glm::rotate(cube3_model, 0.3f, glm::vec3(0,1,0));
+    cube3_model = glm::scale(cube3_model, glm::vec3(0.4, 0.1, 0.2));
+    glm::mat4 cube3_normal = glm::transpose(glm::inverse(glm::mat3(cube3_model)));
+
+    glm::mat4 lamp3_model = glm::translate(model, light3_pos);
+    lamp3_model = glm::scale(lamp3_model, glm::vec3(0.05));
+
+    // render loop
+    // --------------------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input processing
+        // --------------------
+        processInput(window, view_dir, view_loc, perspective_fov, near_z, far_z);
+        saveScreenshotToFile(SCR_SHOT_PATH, window, renderedTexture, SCR_SHOT_WIDTH, SCR_SHOT_HEIGHT);
+        if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS)
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // toggle wireframe
+
+        // render to texture
+        // --------------------
+        glViewport(0, 0, SCR_SHOT_WIDTH, SCR_SHOT_HEIGHT);
+        glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        view = view_dir * view_loc;
+        perspective = makePerspectiveMatrix(perspective_fov, (float)SCR_WIDTH / (float)SCR_HEIGHT, near_z, far_z);
+
+        lightShader.use();
+        lightShader.setMat4("view", view);
+        lightShader.setMat4("perspective", perspective); 
+
+        light1_col = glm::vec3(0.5f, 0.2f, 0.2f);
+        light1_col *= sin(0.125 * glfwGetTime()) + 2;
+        light1_pos = glm::vec3(1.5 + 1.5 * (sin(0.5 * glfwGetTime()) - 1.), 0., 1.2 + 1.2 * (cos(0.5 * glfwGetTime()) - 1.));
+        lamp1_model = glm::translate(model, light1_pos);
+        lamp1_model = glm::scale(lamp1_model, glm::vec3(0.05));
+
+        lightShader.setMat4("model", lamp1_model);
+        lightShader.setVec3("light_col", light1_col);
+        cube.render();
+
+        light2_col = glm::vec3(0.2f, 0.2f, 0.5f);
+        light2_col *= sin(0.125 * glfwGetTime() + 3.14) + 2;
+        light2_pos = glm::vec3(1.5 + 1.5 * (sin(0.5 * glfwGetTime() + 3.14) - 1.), 0., 1.2 + 1.2 * (cos(0.5 * glfwGetTime() + 3.14) - 1.));
+        lamp2_model = glm::translate(model, light2_pos);
+        lamp2_model = glm::scale(lamp2_model, glm::vec3(0.05));
+
+        lightShader.setMat4("model", lamp2_model);
+        lightShader.setVec3("light_col", light2_col);
+        cube.render();
+
+        lightShader.setMat4("model", lamp3_model);
+        lightShader.setVec3("light_col", light3_col);
+        cube.render();
+
+        lightShader.setMat4("model", glm::scale(model, glm::vec3(0.05)));
+        lightShader.setVec3("light_col", glm::vec3(0.f));
+        cube.render();
+
+        ourShader.use();
+        ourShader.setVec3("pointlights[0].pos", light1_pos);
+        ourShader.setVec3("pointlights[0].ambient", light1_col * 0.6f);
+        ourShader.setVec3("pointlights[0].diffuse", light1_col * 0.9f);
+
+        ourShader.setVec3("pointlights[1].pos", light2_pos);
+        ourShader.setVec3("pointlights[1].ambient", light2_col * 0.6f);
+        ourShader.setVec3("pointlights[1].diffuse", light2_col * 0.9f);
+
+        ourShader.setMat4("view", view);
+        ourShader.setMat4("perspective", perspective); 
+        ourShader.setVec3("view_pos", -glm::vec3(view_loc[3]));
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
+
+        ourShader.setMat4("model", cube1_model);
+        ourShader.setMat3("norm_mat", cube1_normal);
+        // obsidian (http://devernay.free.fr/cours/opengl/materials.html)
+        ourShader.setFloat("material.shininess", 128 * .3);
+        cube.render();
+
+        ourShader.setMat4("model", cube2_model);
+        ourShader.setMat3("norm_mat", cube2_normal);
+        // pearl
+        ourShader.setFloat("material.shininess", 128 * 0.088);
+        cube.render();
+
+        ourShader.setMat4("model", cube3_model);
+        ourShader.setMat3("norm_mat", cube3_normal);
+        // chrome
+        ourShader.setFloat("material.shininess", 128 * .6);
+        cube.render();
+
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        ourShader.setMat4("model", table1_model);
+        ourShader.setMat3("norm_mat", table1_normal);
+        // gold
+        ourShader.setFloat("material.shininess", 128 * .4);
+        table.render();
+
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); // disable wireframe
+
+        // render to screen
+        // --------------------
+        glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, renderedTexture);
+        post_shader.use();
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        screen_quad.render();
+
+        // poll for inputs and display screen buffer
+        // --------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    screen_quad.deallocate();
+    table.deallocate();
+    cube.deallocate();
+
+    glfwTerminate();
+    return 0;
+}
+
+unsigned int loadTexture(std::string filename, GLenum image_type) {
+    unsigned int tex_id;
+    glGenTextures(1, &tex_id);
+    glBindTexture(GL_TEXTURE_2D, tex_id); 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true);
+    unsigned char *data = stbi_load(filename.c_str(), &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, image_type, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture: " << filename << std::endl;
+    }
+    stbi_image_free(data);
+    return tex_id;
+}
+
+void processInput(GLFWwindow *window, glm::mat4& view_dir, glm::mat4& view_loc, float& fov, float& near_z, float& far_z)
+{
+    float spd = 0.0005f;
+
+    if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+        spd *= 5;
+    // camera movement
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        view_loc = glm::translate(view_loc, spd * glm::vec3(view_dir[0][0], view_dir[1][0], view_dir[2][0]));
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        view_loc = glm::translate(view_loc, -spd * glm::vec3(view_dir[0][0], view_dir[1][0], view_dir[2][0]));
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        view_loc = glm::translate(view_loc, spd * glm::vec3(view_dir[0][2], view_dir[1][2], view_dir[2][2]));
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        view_loc = glm::translate(view_loc, -spd * glm::vec3(view_dir[0][2], view_dir[1][2], view_dir[2][2]));
+    if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS)
+        view_loc = glm::translate(view_loc, spd * glm::vec3(view_dir[0][1], view_dir[1][1], view_dir[2][1]));
+    if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS)
+        view_loc = glm::translate(view_loc, -spd * glm::vec3(view_dir[0][1], view_dir[1][1], view_dir[2][1]));
+    // camera rotation
+    if (glfwGetKey(window, GLFW_KEY_LEFT) == GLFW_PRESS)
+        view_dir = glm::rotate(view_dir, spd, -glm::vec3(view_dir[0][1], view_dir[1][1], view_dir[2][1]));
+    if (glfwGetKey(window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+        view_dir = glm::rotate(view_dir, spd, glm::vec3(view_dir[0][1], view_dir[1][1], view_dir[2][1]));
+    if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+        view_dir = glm::rotate(view_dir, spd, -glm::vec3(view_dir[0][0], view_dir[1][0], view_dir[2][0]));
+    if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+        view_dir = glm::rotate(view_dir, spd, glm::vec3(view_dir[0][0], view_dir[1][0], view_dir[2][0]));
+    if (glfwGetKey(window, GLFW_KEY_H) == GLFW_PRESS)
+        view_dir = glm::rotate(view_dir, spd, glm::vec3(view_dir[0][2], view_dir[1][2], view_dir[2][2]));
+    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS)
+        view_dir = glm::rotate(view_dir, spd, -glm::vec3(view_dir[0][2], view_dir[1][2], view_dir[2][2]));
+    // camera zoom
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+        fov += spd * 0.5f;
+    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+        fov -= spd * 0.5f;
+    // reset camera position
+    if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS)
+        view_loc = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+    // reset camera rotation
+    if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS)
+        view_dir = glm::mat4(1.0f);
+    // reset camera fov
+    if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS)
+        fov = 0.785f;
+    // near and far plane distances
+    if (glfwGetKey(window, GLFW_KEY_V) == GLFW_PRESS)
+        near_z -= spd;
+    if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS)
+        near_z += spd;
+    if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS)
+        far_z -= 10 * spd;
+    if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS)
+        far_z += 10 * spd;
+}
+
+// recreation of glm's perspective matrix with (l, r, b, t) inputs
+glm::mat4 makePerspectiveMatrix(float n, float f, float l, float r, float b, float t) {
+    glm::mat4 perspective = glm::mat4();
+    // remember that glm does matrix[c][r] format
+    perspective[0] = glm::vec4(2 * n / (r - l), 0.0f, 0.0f, 0.0f);
+    perspective[1] = glm::vec4(0.0f, 2 * n / (t - b), 0.0f, 0.0f);
+    perspective[2] = glm::vec4((r+l)/(r-l), (t+b)/(t-b), -(f+n)/(f-n), -1.0f);
+    perspective[3] = glm::vec4(0.0f, 0.0f, -(2 * f * n)/(f-n), 0.0f);
+    return perspective;
+}
+
+// recreation of glm's glm::perspective()
+glm::mat4 makePerspectiveMatrix(float fov, float aspect_ratio, float n, float f) {
+    float t = tan(fov * 0.5) * n;
+    float r = t * aspect_ratio;
+    return makePerspectiveMatrix(n, f, -r, r, -t, t);
+}
+
+void saveScreenshotToFile(std::string filename, GLFWwindow *window, int renderedTexture, int scr_width, int scr_height) {    
+    if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
+    {
+        std::cout << "argsataerra" << std::endl;
+        std::unique_ptr<unsigned char[]> data = std::make_unique<unsigned char[]>(scr_width * scr_height * 4 * sizeof(unsigned int));
+        glBindTexture(GL_TEXTURE_2D, renderedTexture);
+
+        glPixelStorei(GL_PACK_ALIGNMENT, 1);
+        glGetTexImage(GL_TEXTURE_2D, 0, GL_RGBA, GL_UNSIGNED_BYTE, data.get());
+
+        stbi_flip_vertically_on_write(true);
+        int ret = stbi_write_png(filename.c_str(), scr_width, scr_height, 4, data.get(), scr_width * 4);
+        if (ret == 0)
+        {
+            std::cout << "Failed to capture screenshot" << std::endl;
+        }
+    }
+}
---
+++ Primitive_Lights/src/shader_frag.fs
@@ -0,0 +1,108 @@
+#version 330 core
+
+layout(location = 0) out vec3 FragColor;
+
+in vec2 TexCoord;
+in vec3 FragCoord;
+in vec3 Normal;
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float shininess;
+};
+
+struct PointLight {
+    vec3 pos;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float linear_decay;
+    float exp_decay;
+};
+
+struct Spotlight {
+    vec3 pos;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float umbra;
+    float penumbra;
+    float linear_decay;
+    float exp_decay;
+};
+
+#define NR_POINT_LIGHTS 2
+#define NR_SPOTLIGHTS 1
+
+uniform vec3 view_pos;
+uniform Material material;
+uniform PointLight pointlights[NR_POINT_LIGHTS];
+uniform Spotlight spotlights[NR_SPOTLIGHTS];
+
+vec3 calc_point_light(PointLight light, float shininess, vec3 diffuse_tex, vec3 specular_tex);
+vec3 calc_spotlight(Spotlight light, float shininess, vec3 diffuse_tex, vec3 specular_tex);
+
+void main()
+{
+    vec3 diffuse_tex = texture(material.diffuse, TexCoord).xyz;
+    vec3 specular_tex = texture(material.specular, TexCoord).xyz;
+    vec3 light = vec3(0);
+    for (int i=0; i < NR_POINT_LIGHTS; i++) {
+        light += calc_point_light(pointlights[i], material.shininess, diffuse_tex, specular_tex);
+    }
+    for (int i=0; i < NR_SPOTLIGHTS; i++) {
+        light += calc_spotlight(spotlights[i], material.shininess, diffuse_tex, specular_tex);
+    }
+
+    FragColor = light;
+}
+
+vec3 calc_point_light(PointLight light, float shininess, vec3 diffuse_tex, vec3 specular_tex)
+{
+    // lighting that is always there
+    vec3 ambient = diffuse_tex * light.ambient;
+
+    // light projected from the light source
+    vec3 light_dir = light.pos - FragCoord;
+    float light_dist = sqrt(light_dir.x * light_dir.x + light_dir.y * light_dir.y + light_dir.z * light_dir.z);
+    light_dir = normalize(light_dir);
+    vec3 diffuse = max(dot(Normal, light_dir), 0.0) * diffuse_tex * light.diffuse;
+
+    // reflective lighting from light source
+    vec3 view_dir = normalize(view_pos - FragCoord);
+    vec3 reflect_dir = reflect(-light_dir, Normal);
+    vec3 specular = pow(max(dot(view_dir, reflect_dir), 0.0), shininess) * specular_tex * light.specular;
+
+    // original OpenGL lighting model equation for realistic light falloff
+    // technically the "1" is another constant you can tune, no one does
+    float attenuation = 1 + light.linear_decay * light_dist + light.exp_decay * light_dist * light_dist;
+
+    return (ambient + diffuse + specular) / attenuation;
+}
+
+vec3 calc_spotlight(Spotlight light, float shininess, vec3 diffuse_tex, vec3 specular_tex)
+{
+    // lighting that is always there
+    vec3 ambient = diffuse_tex * light.ambient;
+
+    // light projected from the light source
+    vec3 light_dir = light.pos;
+    float light_dist = sqrt(light_dir.x * light_dir.x + light_dir.y * light_dir.y + light_dir.z * light_dir.z);
+    light_dir = normalize(light_dir);
+    vec3 diffuse = max(dot(Normal, light_dir), 0.0) * diffuse_tex * light.diffuse;
+
+    // reflective lighting from light source
+    vec3 view_dir = normalize(view_pos - FragCoord);
+    vec3 reflect_dir = reflect(-light_dir, Normal);
+    vec3 specular = pow(max(dot(view_dir, reflect_dir), 0.0), shininess) * specular_tex * light.specular;
+
+    // original OpenGL lighting model equation for realistic light falloff
+    // technically the "1" is another constant you can tune, no one does
+    float attenuation = 1 + light.linear_decay * light_dist + light.exp_decay * light_dist * light_dist;
+
+    vec3 col = (ambient + diffuse + specular) / attenuation;
+    float dir = clamp((dot(normalize(FragCoord - light.pos), -light_dir) - light.umbra)/(light.penumbra - light.umbra), 0.f, 1.f);
+    dir = dir * dir * (3 - 2 * dir); // smoothstep
+    return col * dir;
+}
